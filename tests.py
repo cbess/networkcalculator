0eaca7167df2874f91579e50b0ad8c0a44c19f0c
C. Bess <cbess@quantumquinn.com>
2012-04-07 00:00:55 -0500

    - added unit test

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 # ref: http://www.numion.com/calculators/
 import unittest
+import calc
+from pdb import set_trace
 
 def run_all(test_case):
     """Runs all the tests for the specified test case
@@ -10,9 +12,14 @@ def run_all(test_case):
     pass
 
 
-class TestObject(unittest.TestCase):
-    pass
+class TestCalc(unittest.TestCase):
+    def test_bandwidth_formula_fail(self):
+        formula = calc.BandwidthFormulaItem()
+        formula.users_per_day = 7
+        success, message = formula.validate()
+        self.assertFalse(success, 'should fail valid')
+        pass
 
 
 if __name__ == "__main__":
-    run_all(TestObject)
+    run_all(TestCalc)
